94951f70aca94368f7474501104c6fd224afea30
Akhileshrangani4 <akhileshrangani4@gmail.com>
2025-04-02 20:21:34 -0400

    chore: add create-next-app package and release configurations

---
+++ create-tambo-app/src/index.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import { spawn } from 'child_process';
+
+// Use npx to ensure we get the latest version of tambo
+const args = ['tambo', 'create-app', ...process.argv.slice(2)];
+const child = spawn('npx', args, {
+  stdio: 'inherit',
+  shell: true
+});
+
+child.on('exit', (code) => {
+  process.exit(code ?? 0);
+});
