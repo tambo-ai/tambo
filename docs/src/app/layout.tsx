a23e5626b4fcdcad93faebab5e07faf43f99ce3e
michael <mike.michaelmilstead@gmail.com>
2025-07-23 11:28:30 -0700

    force light theme

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,17 +1,24 @@
-import '@/app/global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
+import "@/app/global.css";
+import { RootProvider } from "fumadocs-ui/provider";
+import { ThemeProvider } from "next-themes";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem={false}
+        >
+          <RootProvider>{children}</RootProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
