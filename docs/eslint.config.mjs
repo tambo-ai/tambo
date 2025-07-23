908fd3b2d01f59d078d34eef909b2150ae4d33b8
michael <mike.michaelmilstead@gmail.com>
2025-07-23 13:31:34 -0700

    remove unneeded eslint config

--- docs/eslint.config.mjs
+++
@@ -1,4 +0,0 @@
-import nextJsConfig from "@tambo-ai-cloud/eslint-config/next-js";
-import tseslint from "typescript-eslint";
-
-export default tseslint.config(...nextJsConfig, { ignores: [".source/"] });
