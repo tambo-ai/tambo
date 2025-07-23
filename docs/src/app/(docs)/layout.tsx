19895da55b25565fc830e67062be205750f95dd4
michael <mike.michaelmilstead@gmail.com>
2025-07-23 11:54:20 -0700

    prettier

--- docs/src/app/(docs)/layout.tsx
+++ docs/src/app/(docs)/layout.tsx
@@ -1,12 +1,16 @@
-import '@/app/(docs)/styles.css';
-import { baseOptions } from '@/app/layout.config';
-import { source } from '@/lib/source';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import type { ReactNode } from 'react';
+import "@/app/(docs)/styles.css";
+import { baseOptions } from "@/app/layout.config";
+import { source } from "@/lib/source";
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import type { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions} sidebar={{ collapsible: false }}>
+    <DocsLayout
+      tree={source.pageTree}
+      {...baseOptions}
+      sidebar={{ collapsible: false }}
+    >
       {children}
     </DocsLayout>
   );
--- docs/src/app/layout.config.tsx
+++ docs/src/app/layout.config.tsx
@@ -1,5 +1,5 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import Image from 'next/image';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
 
 export const baseOptions: BaseLayoutProps = {
   nav: {
