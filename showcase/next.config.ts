22b3862cdefbe5d53425da0f7ad0167698847d09
Daniel Gabriel <56942108+revogabe@users.noreply.github.com>
2025-07-28 13:50:09 -0300

    feat: Add InteractiveMap component using react‑leaflet (#678)
    Co-authored-by: Alec Flett <flett@tambo.co>

--- showcase/next.config.ts
+++ showcase/next.config.ts
@@ -1,6 +1,9 @@
 import { type NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // We need to disable reactStrictMode because react-leaflet uses a global
+  // that doesn't work with strict mode.
+  reactStrictMode: false,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
