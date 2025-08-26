c2c972251ce919890600ff61495b24c0c7e3dea9
Arjun Aditya <meow@nermalcat69.dev>
2025-08-27 05:04:39 +0530

    chore: fix weird spacing of ol & ul

--- cli/src/registry/message/message.tsx
+++ cli/src/registry/message/message.tsx
@@ -200,7 +200,7 @@ const MessageContent = React.forwardRef<HTMLDivElement, MessageContentProps>(
       <div
         ref={ref}
         className={cn(
-          "relative block rounded-3xl px-4 py-2 text-[15px] leading-relaxed transition-all duration-200 font-medium max-w-full [&_p]:py-1 [&_ul]:py-4 [&_ol]:py-4 [&_li]:list-item",
+          "relative block rounded-3xl px-4 py-2 text-[15px] leading-relaxed transition-all duration-200 font-medium max-w-full [&_p]:py-1 [&_li]:list-item",
           className,
         )}
         data-slot="message-content"
