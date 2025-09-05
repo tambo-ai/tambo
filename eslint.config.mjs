5c60bba02c1f73ed95f0ea3e8ef3b4b5c972dc04
Arjun Aditya <meow@nermalcat69.dev>
2025-09-05 23:18:59 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
 import config from "@tambo-ai/eslint-config/base";
-import { globalIgnores } from "eslint/config";
 
-export default [...config, globalIgnores([".github/scripts/**"])];
+export default config;
