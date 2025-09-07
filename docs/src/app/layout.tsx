93c24ede8bf3cb828c9fd92636c50773413bdb85
Akhilesh Rangani <akhileshrangani4@gmail.com>
2025-09-07 13:54:09 -0400

    feat: add ask to tambo in the ai actions dropdown (#993)

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,13 +1,15 @@
 import "@/app/global.css";
-import { TamboProvider } from "@tambo-ai/react";
 import { RootProvider } from "fumadocs-ui/provider";
 import { ThemeProvider } from "next-themes";
 import { Inter } from "next/font/google";
-import { components } from "@/lib/tambo";
-import { PostHogPageview, PHProvider } from "@/app/providers";
+import {
+  PostHogPageview,
+  PostHogRootProvider,
+} from "@/providers/posthog-provider";
 import { Suspense } from "react";
 import { WebVitalsReporter } from "@/components/web-vitals";
 import type { Metadata } from "next";
+import { TamboRootProvider } from "@/providers/tambo-provider";
 
 const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://docs.tambo.ai";
 
@@ -54,18 +56,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <TamboProvider
-          apiKey={process.env.NEXT_PUBLIC_TAMBO_API_KEY!}
-          tamboUrl={process.env.NEXT_PUBLIC_TAMBO_URL!}
-          components={components}
-        >
+        <TamboRootProvider>
           <Suspense>
             <PostHogPageview />
           </Suspense>
           <Suspense>
             <WebVitalsReporter />
           </Suspense>
-          <PHProvider>
+          <PostHogRootProvider>
             <ThemeProvider
               attribute="class"
               defaultTheme="light"
@@ -73,8 +71,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             >
               <RootProvider>{children as React.ReactNode}</RootProvider>
             </ThemeProvider>
-          </PHProvider>
-        </TamboProvider>
+          </PostHogRootProvider>
+        </TamboRootProvider>
       </body>
     </html>
   );
