cc208425505cb1893a061ecf50d89685127a216a
Alec Flett <flett@tambo.co>
2025-11-03 16:53:56 -0800

    test(cli): Add testing infra for cli, test some simple commands (#1248)

---
+++ cli/jest.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  testMatch: ["**/tests/**/*.test.ts"],
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+};
+
+export default config;
