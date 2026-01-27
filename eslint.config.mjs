736e8f04f91d0868f491a2b39528329c402133c5
vishal <vishalkumarvkvk988@gmail.com>
2026-01-28 02:34:33 +0530

    feat(template): Template landing page done with shadcn and nextjs.  Using all the fonts hero elements i.e. videos and somewhat styling present in the apps/web

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,9 @@
 import config from "@tambo-ai/eslint-config/base";
 
-export default config;
+export default [
+  ...config,
+  {
+    // Ignore this template folder
+    ignores: ["community/templates/tambo-nextjs-fullstack-starter/**"],
+  },
+];
