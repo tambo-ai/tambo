6038f4fafc11cbdf813e30a2c6f26adbfd0b2c26
vishal <vishalkumarvkvk988@gmail.com>
2026-01-28 02:43:26 +0530

    fix(template): continuation of the first commit ; just changing the ui

--- community/templates/tambo-nextjs-fullstack-starter/eslint.config.mjs
+++ community/templates/tambo-nextjs-fullstack-starter/eslint.config.mjs
@@ -12,8 +12,6 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
-    "**/*.mjs",
-    "eslint.config.mjs",
     "eslint.config.mjs",
     "postcss.config.mjs",
   ]),
