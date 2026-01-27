84e67455cbd1934161773e4d7239dc49a4df287f
vishal <vishalkumarvkvk988@gmail.com>
2026-01-28 03:33:19 +0530

    feat(template): changes after the main branch and added auth.js and reverted back the eslint.config.mjs file to it's origin nature

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,3 @@
 import config from "@tambo-ai/eslint-config/base";
 
-export default [
-  ...config,
-  {
-    // Ignore this template folder
-    ignores: ["community/templates/tambo-nextjs-fullstack-starter/**"],
-  },
-];
+export default config;
