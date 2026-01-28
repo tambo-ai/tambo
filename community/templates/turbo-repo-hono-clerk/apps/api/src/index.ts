9c20348c76196e00ef3235899ba9735c2f54ff3b
Manav Behera <manavbehera123@gmail.com>
2026-01-28 19:39:28 +0530

    feat(clerk):add clerk basic setup

--- community/templates/turbo-repo-hono-clerk/apps/api/src/index.ts
+++ community/templates/turbo-repo-hono-clerk/apps/api/src/index.ts
@@ -1,25 +1,40 @@
-import cors from 'cors';
-import express, { Request, Response } from 'express';
-import { ENV_PARSER } from './utils/configEnv';
+import {
+  clerkClient,
+  clerkMiddleware,
+  getAuth,
+  requireAuth,
+} from "@clerk/express";
+import cors from "cors";
+import express, { Request, Response } from "express";
+import { ENV_PARSER } from "./utils/configEnv";
 
 const app: express.Application = express();
 
+app.use(clerkMiddleware());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: ENV_PARSER.CLIENT_URL,
-}))
+app.use(
+  cors({
+    origin: ENV_PARSER.CLIENT_URL,
+  }),
+);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from the API!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from the API!");
 });
 
+app.get("/protected", requireAuth(), async (req, res) => {
+  const { userId } = getAuth(req);
+  if (!userId) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+  const user = await clerkClient.users.getUser(userId);
 
-
-
-
+  return res.json({ user });
+});
 
 const PORT = ENV_PARSER.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`API server is running on http://localhost:${PORT}`);
-}); 
+});
